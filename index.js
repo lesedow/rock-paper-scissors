3f479fdf8babd29eb0910098e3b9a64bd88e74e9
lesedow <lesedow@gmail.com>
2023-09-11 19:08:53 +0300

    Create the playRound function
    Create a function named playRound
    that takes two parameters (compChoice,
    playerChoice).
    
    This function compares the two choices
    by using the rock, paper, scissors rules
    in order to determine a verdict for the
    round.

--- index.js
+++ index.js
@@ -5,3 +5,45 @@ function getCompChoice() {
     let compChoice = availableChoices[randomNumber];
     return compChoice;
 }
+
+function playRound (compChoice, playerChoice) {
+    playerChoice = playerChoice.toLowerCase();
+    let verdict = `${playerChoice} vs ${compChoice} | `;
+
+    if (
+        playerChoice !== 'rock' && 
+        playerChoice !== 'paper' && 
+        playerChoice !== 'scissors'
+    ) {
+        verdict = 'Your choice is invalid!';
+        return verdict;
+    }
+     
+    if (playerChoice === compChoice) {
+        verdict = `${playerChoice} vs ${compChoice} | It's a draw!`;
+        return verdict;
+    }
+
+    if (playerChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            verdict += 'You lose! Paper beats Rock.';
+        } else {
+            verdict += 'You win! Rock beats Scissors.';
+        } 
+    } else if (playerChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+            verdict += 'You lose! Scissors beats Paper.';
+        } else {
+            verdict += 'You win! Paper beats Rock.';
+        }
+    } else {
+        if (computerChoice === 'rock') {
+            verdict += 'You lose! Rock beats Scissors.';
+        } else {
+            verdict += 'You win! Paper beats Rock.';
+        }
+    }
+
+
+    return verdict;
+}
