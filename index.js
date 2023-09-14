4135c393a94829e6ebed63ecdf30177af0034f20
lesedow <lesedow@gmail.com>
2023-09-14 19:16:12 +0300

    Make setVerdict()
    Make a function that displays
    the verdict of the round on screen.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+import { playRound, getScore } from './gameLogic.js';
+
 const gameContainer = document.getElementById('game-container');
 const scoreElement = document.getElementById('score');
+const verdictElement = document.getElementById('verdict');
 
 function getPlayerChoice (event) {
     const dataChoiceAttribute = event.target.getAttribute('data-choice');
     if (!dataChoiceAttribute) return;
 
     const verdict = playRound(dataChoiceAttribute);
-    setVerdict(verdit);
+    setVerdict(verdict);
 }
 
 function updateScore () {
@@ -14,7 +17,7 @@ function updateScore () {
 }
 
 function setVerdict (verdict) {
-
+    verdictElement.textContent = verdict;
 }
  
 gameContainer.addEventListener('click', getPlayerChoice);
