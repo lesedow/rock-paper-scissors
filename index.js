eda83bd4bd603aa0d60ce2704faa19c9f4b5e058
lesedow <lesedow@gmail.com>
2023-09-14 19:04:29 +0300

    Get player choice
    Listen for click on the parent
    of the buttons, call getPlayerChoice
    function and play a round with that choice

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const gameContainer = document.getElementById('game-container');
+const scoreElement = document.getElementById('score');
 
+function getPlayerChoice (event) {
+    const dataChoiceAttribute = event.target.getAttribute('data-choice');
+    if (!dataChoiceAttribute) return;
+
+    const verdict = playRound(dataChoiceAttribute);
+    setVerdict(verdit);
+}
+
+function updateScore () {
+
+}
+
+function setVerdict (verdict) {
+
+}
+ 
+gameContainer.addEventListener('click', getPlayerChoice);
